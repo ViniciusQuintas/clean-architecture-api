6ee1605f213df1472db6597525349e5ca9a34ecc
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-21 15:16:10 -0300

    chore: rename folders and files

--- src/usecases/register-user-on-mailing-list/ports/user-repostirory.ts
+++ src/usecases/register-user-on-mailing-list/ports/user-repostirory.ts
@@ -1,4 +1,4 @@
-import { UserData } from '../register-user-on-maling-list/user-data'
+import { UserData } from '../user-data'
 
 export interface UserRepository{
   add(user: UserData): Promise<void>
--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { UserData } from './user-data'
 
 describe('Register user on mailing list', () => {
-  test('should add user with complete data to mailing list', async () => {
+  test('should add user with complete data to mailing list', () => {
     const users: UserData[] = []
     console.log(users)
     // const repo: UserRepository = new InMemoryUserRepository(user)
     // const usecase: RegisterUserOrMailingList = new RegisterUserOnMailngList(repo)
     // const name ='any_name'
-    // const email = 'any@email.com'
+    // const email = 'any@mail.com'
     // const response = await usecase.registerUserOnMailngList({ name, email })
-    // const user = repo.findUserByEmail('any@gmail.com')
+    // const user = repo.findUserByEmail('any@mail.com')
     // expect((await user).name).toBe('any_name')
   })
 })
