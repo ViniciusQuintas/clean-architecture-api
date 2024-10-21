bf56309efa32393226ae10142af037ca6d5f3573
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-21 14:09:20 -0300

    test: should add user with complete data to mailing list

--- src/usecases/register-user-on-maling-list/register-user-on-maling-list.spec.ts
+++ src/usecases/register-user-on-maling-list/register-user-on-maling-list.spec.ts
@@ -1,5 +1,15 @@
-describe('Register user on meailing list', () => {
-  test('', () => {
-    expect(1).toBe(1)
+import { UserData } from './user-data'
+
+describe('Register user on mailing list', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(user)
+    // const usecase: RegisterUserOrMailingList = new RegisterUserOnMailngList(repo)
+    // const name ='any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.registerUserOnMailngList({ name, email })
+    // const user = repo.findUserByEmail('any@gmail.com')
+    // expect((await user).name).toBe('any_name')
   })
 })
