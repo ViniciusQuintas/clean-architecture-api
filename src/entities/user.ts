d4bbf17c53b0f9d1f3601c1bc3181cc27b905da5
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-22 15:54:25 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
@@ -7,6 +7,14 @@ import { Name } from './name'
 import { UserData } from './user-data'
 
 export class User {
+  public readonly name: Name
+  public readonly email: Email
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
@@ -14,9 +22,13 @@ export class User {
     }
 
     const emailOrError = Email.create(userData.email)
-
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value
+    const email: Email = emailOrError.value
+
+    return right(new User(name, email))
   }
 }
