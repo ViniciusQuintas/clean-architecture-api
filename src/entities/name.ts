aabcc6742dbff30aef0be452e9093adb525f6550
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-22 15:39:32 -0300

    feat: verify name while creating user

---
+++ src/entities/name.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+import { Either, left, right } from '../shared/either'
+import { InvalidNameError } from './errors/invalid-name-error'
+
+export class Name {
+  private readonly value: string
+
+  private constructor (name: string) {
+    this.value = name
+  }
+
+  public static create (name: string): Either<InvalidNameError, Name> {
+    if (!Name.validade(name)) {
+      return left(new InvalidNameError())
+    }
+
+    return right(new Name(name))
+  }
+
+  public static validade (name: string): boolean {
+    if (!name) {
+      return false
+    }
+
+    if (name.trim().length < 2 || name.trim().length > 256) {
+      return false
+    }
+
+    return true
+  }
+}
