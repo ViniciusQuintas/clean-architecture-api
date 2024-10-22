cedb5fc1b1edb6223bc9cc3a9ece837cf79c3cf9
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-22 15:39:45 -0300

    feat: verify name while creating user

--- src/entities/name.ts
+++ src/entities/name.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 import { Either, left, right } from '../shared/either'
 import { InvalidNameError } from './errors/invalid-name-error'
