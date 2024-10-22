756e39d7695c54ec4e2261e316cbb5abdd84012a
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-22 11:20:11 -0300

    test: validade email with domain part with invalid char

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -45,4 +45,9 @@ describe('Email validation', () => {
     const email = `any@${'d'.repeat(64)}.com`
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty local part with invalid chart', () => {
+    const email = 'any email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
