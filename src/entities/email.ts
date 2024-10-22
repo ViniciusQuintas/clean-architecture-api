85e405ed2e7df5d0c4a6673ce34657469e9153c6
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-22 09:24:52 -0300

    feat: validade domain emails with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -11,8 +11,18 @@ export class Email {
     }
 
     const [local, domain] = email.split('@')
+
     if (local.length > 64 || local.length === 0) { return false }
+
     if (domain.length > 255 || domain.length === 0) return false
+
+    const domainParts = domain.split('.')
+    if (domainParts.some((part) => {
+      return part.length > 63
+    })) {
+      return false
+    }
+
     return true
   }
 }
