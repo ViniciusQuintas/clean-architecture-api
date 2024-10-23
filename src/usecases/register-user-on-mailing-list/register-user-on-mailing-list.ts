bb9fc365ca2e90a4ba08e99fad0ca65bd01ebfd8
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-23 10:29:26 -0300

    feat: register valid user to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/promise-function-async */
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+import { User } from '../../entities/user'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared/either'
+import { UserRepository } from './ports/user-repostirory'
+
+export class RegisterUserOrMailingList {
+  private readonly userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async registerUserOrMailingList (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+
+    return right(request)
+  }
+}
