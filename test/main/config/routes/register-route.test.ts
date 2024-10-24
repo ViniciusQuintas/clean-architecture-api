59b054cd5afadb8ca7a0656a7512c43384a2afd8
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-24 15:59:35 -0300

    test: config register routes

---
+++ test/main/config/routes/register-route.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('register route', () => {
+  test('should retunr an account on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Any name',
+        email: 'any@mail.com'
+      })
+      .expect(201)
+  })
+})
